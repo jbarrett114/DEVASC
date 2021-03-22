ee73c5e5974050e4f581d647f2d57be291a1ebf5
Jack Barrett <jbarrett114@gmail.com>
2021-03-22 12:11:27 -0400

    Test TEAMS messaging

---
+++ Teams/test-message.py
@@ -0,0 +1,4 @@
+import pymsteams
+myTeamsMessage = pymsteams.connectorcard("https://appriver3651003900.webhook.office.com/webhookb2/38bb4bfd-f8a1-4544-b41c-83fd4ac05ecb@34a23b70-14ed-448c-b453-c1a79a3e5d27/IncomingWebhook/9dff2c17f628417da29bbfb20973c15f/48060f97-19c5-4460-97a0-3668fac3aaea")
+myTeamsMessage.text("THE INTERNETS IN DOWN!!!!")
+myTeamsMessage.send()
